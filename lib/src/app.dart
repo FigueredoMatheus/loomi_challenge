6393a35dd2ecac250f89c25f00ecee5a9e253ed2
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-01 10:22:21 -0300

    Configure Get for route management

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,16 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:loomi_challenge/flavors/flavors.dart';
+import 'package:loomi_challenge/src/core/routes/pages_routes.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: FlavorSettings.title,
+      getPages: PagesRoute.pages,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
