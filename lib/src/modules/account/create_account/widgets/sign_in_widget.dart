0d58263622a6b820b0cd0638f978d22daf280b3a
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-02 19:39:07 -0300

    CreateAccountSignInWidget

---
+++ lib/src/modules/account/create_account/widgets/sign_in_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:loomi_challenge/src/core/themes/my_app_k_colors.dart';
+
+class CreateAccountSignInWidget extends StatelessWidget {
+  const CreateAccountSignInWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: 'Already have an account? ',
+            style: Get.textTheme.bodySmall,
+          ),
+          TextSpan(
+              text: 'Sign In!',
+              style: Get.textTheme.bodyMedium!.copyWith(
+                fontWeight: FontWeight.w700,
+                color: MyAppKColors.kPurpleColor,
+              ),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  print('TAPPED');
+                }),
+        ],
+      ),
+    );
+  }
+}
