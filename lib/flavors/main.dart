45b22ea127bd1091428e45e1452c5a73c67325d5
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-02 18:30:21 -0300

    Fix Firebase Initialize

--- lib/flavors/main.dart
+++ lib/flavors/main.dart
@@ -1,21 +1,24 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:loomi_challenge/src/core/services/firebase_options.dart';
 import 'package:loomi_challenge/src/core/themes/my_app_themes.dart';
 import '../src/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future initServices() async {
   Get.log('Starting services...');
 
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Get.put(MyAppThemes());
 
   Get.log('All services started...');
 }
 
 FutureOr<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await initServices();
 
   runApp(const MyApp());
