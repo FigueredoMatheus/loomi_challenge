4bc3063b63c0754f13c9fcce9c88dc9b6e11358a
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-03 20:38:24 -0300

    Implement method loginUserEmailPass in AuthRepository

--- lib/src/repositories/auth_repository/auth_repository.dart
+++ lib/src/repositories/auth_repository/auth_repository.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 part 'create_account_email_pass.dart';
+part 'login_use_email_pass.dart';
 
 class AuthRepository {
   Future<Map<String, dynamic>> createUserEmailPass({
@@ -8,4 +9,10 @@ class AuthRepository {
     required String password,
   }) async =>
       implCreateUserEmailPass(email: email, password: password);
+
+  Future<Map<String, dynamic>> loginUserEmailPass({
+    required String email,
+    required String password,
+  }) async =>
+      implLoginUserEmailPass(email: email, password: password);
 }
