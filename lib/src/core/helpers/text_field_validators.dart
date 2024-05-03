8bae92f3192b47431d276540003b54893bb5ecaa
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-03 08:09:55 -0300

    Create class TextFieldValidators to store all valudator rule

---
+++ lib/src/core/helpers/text_field_validators.dart
@@ -0,0 +1,59 @@
+import 'package:email_validator/email_validator.dart';
+
+class TextFieldValidators {
+  static String? emailValidator(String? text) {
+    if (text == null || text.isEmpty) {
+      return 'Email is required ';
+    }
+    final bool isValid = EmailValidator.validate(text);
+
+    if (!isValid) {
+      return 'Invalid Email';
+    }
+
+    return null;
+  }
+
+  static String? passwordValidator(String? text) {
+    if (text == null || text.isEmpty) {
+      return 'Password is required';
+    }
+
+    final bool isValid = text.length < 8;
+
+    if (!isValid) {
+      return 'Password too short';
+    }
+
+    return null;
+  }
+
+  static String? confirmPasswordValidator(String? text, String password) {
+    if (text == null || text.isEmpty) {
+      return 'Confirm your password for security.';
+    }
+
+    final bool isValid = text == password;
+
+    if (!isValid) {
+      return 'Passwords are not the same';
+    }
+
+    return null;
+  }
+
+  static String? nameValidator(String? text) {
+    if (text == null || text.isEmpty) {
+      return 'Name is required';
+    }
+
+    final RegExp nameRegExp = RegExp(r"\w+\s\w+");
+    final isNameValid = nameRegExp.hasMatch(text);
+
+    if (!isNameValid) {
+      return 'First and last name required';
+    }
+
+    return null;
+  }
+}
