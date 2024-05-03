b40b8d80c8610f757f6c688e233277e0deca5cca
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-03 20:42:52 -0300

    Implement GetUserData in FirebaseDatabaseRepository

--- lib/src/repositories/firebase_database_repository/firebase_database_repository.dart
+++ lib/src/repositories/firebase_database_repository/firebase_database_repository.dart
@@ -1,8 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 part 'post_user_data.dart';
+part 'get_user_data.dart';
 
 class FirebaseDatabaseRepository {
+  Future<Map<String, dynamic>> GetUserData(String userId) async =>
+      implGetUserData(userId);
+
   Future<Map<String, dynamic>> postUserData(
     Map<String, dynamic> data,
   ) async =>
