053a7cace9cb7869a88db524f96a7cfc19739f9f
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-04 00:02:04 -0300

    Set createAccountPageView as initialRoute

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:loomi_challenge/flavors/flavors.dart';
 import 'package:loomi_challenge/src/core/routes/pages_routes.dart';
+import 'package:loomi_challenge/src/core/routes/routes_names.dart';
 import 'package:loomi_challenge/src/core/themes/my_app_themes.dart';
-import 'package:loomi_challenge/src/modules/account/login/view/login_page_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       getPages: PagesRoutes.pages,
       debugShowCheckedModeBanner: false,
       theme: Get.find<MyAppThemes>().getLightTheme(),
-      home: LoginPageView(),
+      initialRoute: RoutesNames.createAccountPageView,
     );
   }
 }
