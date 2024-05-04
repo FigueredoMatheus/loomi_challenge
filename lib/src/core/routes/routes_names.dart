0b83a9713a1f121cd2aa1814d3636052303730d6
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-03 23:52:17 -0300

    Implement RoutesNames

--- lib/src/core/routes/routes_names.dart
+++ lib/src/core/routes/routes_names.dart
@@ -1,1 +1,7 @@
-class RoutesNames {}
+class RoutesNames {
+  static const loginPageView = '/login-page_view';
+  static const createAccountPageView = '/create-account-page-view';
+  static const forgotPasswordPageView = '/forgot-password-page-view';
+  static const successOnSendResentEmailPageView =
+      'success-on-send-resent-email-page-view';
+}
