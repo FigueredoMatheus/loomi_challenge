25f5ec0d449af6045ce529bb287bcfba0cfd11fa
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-03 23:29:57 -0300

    Add optinal Width to CustomElevatedButton

--- lib/src/common/widgets/custom_elevated_button.dart
+++ lib/src/common/widgets/custom_elevated_button.dart
@@ -2,19 +2,21 @@ import 'package:flutter/material.dart';
 
 class CustomElevatedButton extends StatelessWidget {
   final String label;
+  final double containerWidth;
   final Function() onPressed;
 
   const CustomElevatedButton({
     super.key,
     required this.label,
     required this.onPressed,
+    this.containerWidth = 207.77,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 41.58,
-      width: 207.77,
+      width: containerWidth,
       child: ElevatedButton(
         onPressed: onPressed,
         child: Text(label),
