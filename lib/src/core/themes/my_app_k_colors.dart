f53c2f5d91506d5539797bd1ddf361953305d61c
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-04 23:04:07 -0300

    Add kMovieCardBorderColor

--- lib/src/core/themes/my_app_k_colors.dart
+++ lib/src/core/themes/my_app_k_colors.dart
@@ -17,6 +17,8 @@ class MyAppKColors {
   static const kFormFieldBgColor = Color(0xFF23252C);
   static const kButtonShowColor = Color(0xFFF1CC4C);
 
+  static const kMovieCardBorderColor = Color(0xFF929292);
+
   static TextStyle title2() {
     return GoogleFonts.epilogue(
       fontSize: 14,
