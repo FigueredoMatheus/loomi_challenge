9da803dd549872ad215d3aad2fac35e7e5adcc5d
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-05 02:23:22 -0300

    Add colors and textstyle in MyAppKColors

--- lib/src/core/themes/my_app_k_colors.dart
+++ lib/src/core/themes/my_app_k_colors.dart
@@ -9,6 +9,7 @@ class MyAppKColors {
   static const kLightGrayColor = Color(0xFFD7D7D7);
   static const kDarkGrayColor = Color(0xFF555252);
   static const kSilverGrayColor = Color(0xFFBBBBBB);
+  static const kGainsboroGrayColor = Color(0xFFD9D9D9);
 
   static const kOffWhiteColor = Color(0xFFF4F4F4);
   static const kWhiteColor = Color(0xFFFFFFFF);
@@ -18,6 +19,18 @@ class MyAppKColors {
   static const kButtonShowColor = Color(0xFFF1CC4C);
 
   static const kMovieCardBorderColor = Color(0xFF929292);
+
+  static const kUserProfileImgeBgColorJ = Color(0xFF32A1DC);
+  static const kUserProfileImgeBgColorL = Color(0xFFF7931A);
+  static const kUserProfileImgeBgColorS = Color(0xFFD9507A);
+
+  static TextStyle title1() {
+    return GoogleFonts.epilogue(
+      fontSize: 18,
+      color: MyAppKColors.kWhiteColor,
+      fontWeight: FontWeight.w700,
+    );
+  }
 
   static TextStyle title2() {
     return GoogleFonts.epilogue(
@@ -55,6 +68,70 @@ class MyAppKColors {
       color: MyAppKColors.kWhiteColor,
       fontSize: 10,
       fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle movieCardGenre() {
+    return GoogleFonts.epilogue(
+      color: MyAppKColors.kWhiteColor,
+      fontSize: 15,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle movieCardTitle() {
+    return GoogleFonts.epilogue(
+      color: MyAppKColors.kWhiteColor,
+      fontSize: 32,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle movieCardSinopse() {
+    return GoogleFonts.epilogue(
+      fontSize: 14,
+      color: MyAppKColors.kWhiteColor,
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle movieCardMostRecentComment() {
+    return GoogleFonts.epilogue(
+      fontSize: 12,
+      color: MyAppKColors.kWhiteColor,
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle movieCardNumberOfComments() {
+    return GoogleFonts.epilogue(
+      fontSize: 12,
+      color: MyAppKColors.kWhiteColor,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle movieCardBottomButton() {
+    return GoogleFonts.epilogue(
+      fontSize: 10,
+      color: MyAppKColors.kWhiteColor.withOpacity(0.45),
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle movieCardBottomExpiresDateLabel() {
+    return GoogleFonts.epilogue(
+      fontSize: 12,
+      color: MyAppKColors.kWhiteColor.withOpacity(0.45),
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle movieCardBottomExpiresDate() {
+    return GoogleFonts.epilogue(
+      fontSize: 14,
+      color: MyAppKColors.kPurpleColor,
+      fontWeight: FontWeight.w600,
     );
   }
 }
