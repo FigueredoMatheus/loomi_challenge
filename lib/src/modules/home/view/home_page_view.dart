cccb94241330fc1ae967e27d56e8551278a91230
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-05 10:10:26 -0300

    Add HomePageViewHeader and HomePageViewMovieCard in HomePageViewModel

--- lib/src/modules/home/view/home_page_view.dart
+++ lib/src/modules/home/view/home_page_view.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:loomi_challenge/src/core/data/temp_file.dart';
+import 'package:loomi_challenge/src/modules/home/widgets/movie_card/movie_card.dart';
+import 'package:loomi_challenge/src/modules/home/widgets/page_header.dart';
 
 class HomePageViewModel extends StatelessWidget {
   const HomePageViewModel({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Center(
+        child: Column(
+          children: [
+            HomePageViewHeader(),
+            HomePageViewMovieCard(movie: movie),
+          ],
+        ),
+      ),
+    );
   }
 }
