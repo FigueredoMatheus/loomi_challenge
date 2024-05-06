dd29aa389d1f1154f3166c7487e81586b993d78d
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-05 23:49:56 -0300

    Implement Provider AuthService

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -3,20 +3,24 @@ import 'package:get/get.dart';
 import 'package:loomi_challenge/flavors/flavors.dart';
 import 'package:loomi_challenge/src/core/routes/pages_routes.dart';
 import 'package:loomi_challenge/src/core/routes/routes_names.dart';
+import 'package:loomi_challenge/src/core/services/auth_service.dart';
 import 'package:loomi_challenge/src/core/themes/my_app_themes.dart';
-import 'package:loomi_challenge/src/modules/account/create_account/view/create_user_account.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: FlavorSettings.title,
-      getPages: PagesRoutes.pages,
-      debugShowCheckedModeBanner: false,
-      theme: Get.find<MyAppThemes>().getLightTheme(),
-      initialRoute: RoutesNames.createAccountPageView,
+    return Provider(
+      create: (context) => AuthService(),
+      child: GetMaterialApp(
+        title: FlavorSettings.title,
+        getPages: PagesRoutes.pages,
+        debugShowCheckedModeBanner: false,
+        theme: Get.find<MyAppThemes>().getLightTheme(),
+        initialRoute: RoutesNames.createAccountPageView,
+      ),
     );
   }
 }
