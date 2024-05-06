a4ab8ac9dd18e952bce99ddf72229e8bca9adb70
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-06 05:52:41 -0300

    Implement EditProfilePageViewCustomProfileCardImage

---
+++ lib/src/modules/account/user_settings/widgets/edit_profile/profile_image/profile_card_image.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:loomi_challenge/src/common/widgets/circle_avatar_profile_image.dart';
+import 'package:loomi_challenge/src/core/data/temp_file.dart';
+import 'package:loomi_challenge/src/core/themes/my_app_k_colors.dart';
+
+class EditProfilePageViewCustomProfileCardImage extends StatelessWidget {
+  const EditProfilePageViewCustomProfileCardImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final containerSize = 120.0;
+    final imageContainerSize = containerSize * 0.95;
+    final cameraIconContainerSize = containerSize * 0.40;
+    final cameraIconSize = containerSize * 0.80;
+    return Container(
+      width: containerSize,
+      height: containerSize,
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            child: CircleAvatarProfileImage(
+              user: user,
+              containerSize: imageContainerSize,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Container(
+              width: cameraIconContainerSize,
+              height: cameraIconContainerSize,
+              decoration: BoxDecoration(
+                color: MyAppKColors.kdarkShadeOliveGreen,
+                shape: BoxShape.circle,
+              ),
+              child: SvgPicture.asset(
+                'assets/icons/camera_icon.svg',
+                semanticsLabel: 'Google',
+                fit: BoxFit.scaleDown,
+                height: cameraIconSize,
+                width: cameraIconSize,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
