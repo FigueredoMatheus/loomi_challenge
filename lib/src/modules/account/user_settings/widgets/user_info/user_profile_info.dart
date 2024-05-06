bacda975b05ab5d0ccc7d2785cfae95bbba91a3b
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-06 01:20:26 -0300

    Create UserSettingsPageViewUserProfileInfo

---
+++ lib/src/modules/account/user_settings/widgets/user_info/user_profile_info.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:loomi_challenge/src/common/widgets/circle_avatar_profile_image.dart';
+import 'package:loomi_challenge/src/core/data/temp_file.dart';
+import 'package:loomi_challenge/src/modules/account/user_settings/widgets/user_info/user_profile_name.dart';
+
+class UserSettingsPageViewUserProfileInfo extends StatelessWidget {
+  const UserSettingsPageViewUserProfileInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CircleAvatarProfileImage(user: user, containerSize: 110),
+        const SizedBox(width: 10),
+        Expanded(child: UserSettingsPageViewProfileInfoName()),
+      ],
+    );
+  }
+}
