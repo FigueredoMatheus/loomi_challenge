4d70ee515cb4050c0487c64bb269fba64946cfc3
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-05 23:56:53 -0300

    Implement logged user in CircleAvatarProfileImage

--- lib/src/modules/home/widgets/page_header.dart
+++ lib/src/modules/home/widgets/page_header.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:loomi_challenge/src/common/widgets/my_app_icon.dart';
 import 'package:loomi_challenge/src/core/data/constants.dart';
-import 'package:loomi_challenge/src/core/data/temp_file.dart';
+import 'package:loomi_challenge/src/core/services/auth_service.dart';
 import 'package:loomi_challenge/src/modules/home/widgets/circle_avatar_profile_image.dart';
+import 'package:provider/provider.dart';
 
 class HomePageViewHeader extends StatelessWidget {
   const HomePageViewHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final loggedUser = Provider.of<AuthService>(context).user;
     return Container(
       color: Colors.transparent,
       height: homePageViewCustomAppBarHeight,
@@ -27,7 +29,7 @@ class HomePageViewHeader extends StatelessWidget {
           ),
           Positioned(
             right: 0,
-            child: CircleAvatarProfileImage(user: user),
+            child: CircleAvatarProfileImage(user: loggedUser),
           ),
         ],
       ),
