b4d32acd2eef7d919bb5e636d2ac1cde00d7972c
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-06 05:31:54 -0300

    Create EditProfilePageView

---
+++ lib/src/modules/account/user_settings/view/edit_profile_page_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:loomi_challenge/src/common/widgets/custom_page_header/custom_page_header.dart';
+import 'package:loomi_challenge/src/common/widgets/custom_page_view_title.dart';
+import 'package:loomi_challenge/src/core/data/constants.dart';
+import 'package:loomi_challenge/src/core/data/my_app_enums.dart';
+
+class EditProfilePageView extends StatelessWidget {
+  const EditProfilePageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: MediaQuery.of(context).padding.top + pageViewDefaultPadding,
+            bottom: pageViewDefaultPadding,
+            left: pageViewDefaultPadding,
+            right: pageViewDefaultPadding,
+          ),
+          child: Column(
+            children: [
+              CustomPageViewHeader(
+                  headerType: CustomPageViewHeaderType.editProfile),
+              CustomPageViewTitle(
+                  customPageViewTitleType: CustomPageViewTitleType.editProfile),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
