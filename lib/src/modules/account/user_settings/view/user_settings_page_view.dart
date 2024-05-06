c52ad101421a7fc5a08d9c9d3c6ea7111a961c70
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-06 00:29:43 -0300

    Create UserSettingsPageView

---
+++ lib/src/modules/account/user_settings/view/user_settings_page_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserSettingsPageView extends StatelessWidget {
+  const UserSettingsPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
