07c0229f9dd655ff195d5878d6751047f340947e
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-06 02:23:31 -0300

    implement deleteAccountOnTap on UserSettingsPageViewCustomButtonTile

--- lib/src/modules/account/user_settings/widgets/custom_button_tile.dart
+++ lib/src/modules/account/user_settings/widgets/custom_button_tile.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:loomi_challenge/src/common/utils/open_modal_bottom_sheet.dart';
 import 'package:loomi_challenge/src/core/data/my_app_enums.dart';
 import 'package:loomi_challenge/src/core/themes/my_app_k_colors.dart';
+import 'package:loomi_challenge/src/modules/account/user_settings/widgets/delete_acc_modal_body.dart';
 
 class UserSettingsPageViewCustomButtonTile extends StatelessWidget {
   final CustomButtonTileType buttonTileType;
@@ -89,6 +91,7 @@ class UserSettingsPageViewCustomButtonTile extends StatelessWidget {
   }
 
   deleteAccountOnTap() {
-    print('DELETE ACCOUNT BUTTON TAPPED');
+    openModalBottomSheet(
+        title: 'Warning', child: UserSettingsDeleteAccountModalBottomBody());
   }
 }
