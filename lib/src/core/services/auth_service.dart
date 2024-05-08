7a2bddf08f2e186fca6c14184a5066533c02d569
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-08 13:59:53 -0300

    Implement signUpAccount method in AuthService

--- lib/src/core/services/auth_service.dart
+++ lib/src/core/services/auth_service.dart
@@ -1,8 +1,36 @@
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:loomi_challenge/src/common/utils/dialogs/exception_warning_dialog.dart';
+import 'package:loomi_challenge/src/common/utils/dialogs/loading_dialog.dart';
+import 'package:loomi_challenge/src/core/helpers/dio_exception_helper.dart';
+import 'package:loomi_challenge/src/core/routes/routes_names.dart';
+import 'package:loomi_challenge/src/core/services/get_it.dart';
 import 'package:loomi_challenge/src/models/entity/user_model/user_entity.dart';
+import 'package:loomi_challenge/src/repositories/auth_repository/auth_repository.dart';
+import 'package:provider/provider.dart';
 
 class AuthService {
+  signUpAccount(Map<String, dynamic> data) async {
+    loadingDialog();
+
+    try {
+      final response = await getIt<AuthRepository>().registerUser(data);
+
+      Provider.of<AuthService>(Get.context!, listen: false)
+          .initUser(response.userEntity.toJson());
+
+      Get.offAllNamed(RoutesNames.homePageView);
+    } on DioException catch (exception) {
+      Get.back();
+
+      final exceptionModel = DioExceptionHelper().getException(exception.type);
+
+      exceptionWarning(exceptionModel);
+    }
+  }
+
   static final _auth = FirebaseAuth.instance;
 
   bool get isUserLoggedIn => _auth.currentUser != null;
@@ -18,4 +46,22 @@ class AuthService {
     await GoogleSignIn().signOut();
     user.onLogout();
   }
+  /*
+
+  Future<UserCredential> implGoogleSignIn() async {
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  return await FirebaseAuth.instance.signInWithCredential(credential);
+}
+
+
+  */
 }
