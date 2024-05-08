aaa5d1cff5a8c12ab11c1c76692ec1ef5af1c610
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-08 13:51:40 -0300

    Add AuthRepository to getIt

--- lib/src/core/services/get_it.dart
+++ lib/src/core/services/get_it.dart
@@ -1,8 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:loomi_challenge/src/modules/account/create_account/controller/create_account_controller.dart';
 import 'package:loomi_challenge/src/modules/account/forgot_password/controller/forgot_password_controller.dart';
 import 'package:loomi_challenge/src/modules/account/login/controller/login_controller.dart';
 import 'package:loomi_challenge/src/modules/account/user_settings/controller/settings_controller.dart';
+import 'package:loomi_challenge/src/repositories/auth_repository/auth_repository.dart';
 
 GetIt getIt = GetIt.instance;
 
@@ -11,4 +13,9 @@ void setupGetItClasses() {
   getIt.registerLazySingleton(() => LoginController());
   getIt.registerLazySingleton(() => ForgotPasswordController());
   getIt.registerLazySingleton(() => SettingsController());
+  getIt.registerLazySingleton(() => AuthRepository(Dio(BaseOptions(
+        contentType: 'application/json',
+        connectTimeout: Duration(seconds: 10),
+        receiveTimeout: Duration(seconds: 3),
+      ))));
 }
