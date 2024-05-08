4be5ea286ecf49cf6f6b8341c159b48677bcd909
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-08 13:49:59 -0300

    Create exceptionWarning

---
+++ lib/src/common/utils/dialogs/exception_warning_dialog.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:loomi_challenge/src/core/themes/app_themes.dart';
+import 'package:loomi_challenge/src/models/dio_exception_model.dart';
+
+exceptionWarning(DioExceptionModel exception) {
+  Get.defaultDialog(
+    title: exception.title,
+    middleText: exception.message,
+    backgroundColor: MyThemes.get().kVeryDarkGrayColor,
+    titleStyle: MyThemes.get().epilogueStyle.copyWith(fontSize: 14),
+    middleTextStyle: MyThemes.get().montserratStyle.copyWith(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        color: MyThemes.get().kWhiteColor.withOpacity(0.45)),
+    titlePadding: const EdgeInsets.only(top: 15),
+    radius: 15,
+    actions: [
+      TextButton(
+        child: const Text('Ok'),
+        onPressed: () => Get.back(),
+      ),
+    ],
+  );
+}
