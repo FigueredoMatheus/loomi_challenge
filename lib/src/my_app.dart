8956243f2d21726d1883e88c5681a163759f6d84
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-09 01:28:54 -0300

    Create HomePageBloc in BlocProvider

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -5,21 +5,26 @@ import 'package:loomi_challenge/src/core/routes/pages_routes.dart';
 import 'package:loomi_challenge/src/core/routes/routes_names.dart';
 import 'package:loomi_challenge/src/core/services/auth_service.dart';
 import 'package:loomi_challenge/src/core/themes/my_app_themes.dart';
+import 'package:loomi_challenge/src/modules/home/bloc/bloc/home_page_bloc.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Provider(
-      create: (context) => AuthService(),
-      child: GetMaterialApp(
-        title: FlavorSettings.title,
-        getPages: PagesRoutes.pages,
-        debugShowCheckedModeBanner: false,
-        theme: Get.find<MyAppThemes>().getLightTheme(),
-        initialRoute: RoutesNames.createAccountPageView,
+    return BlocProvider(
+      create: (_) => HomePageBloc(),
+      child: Provider(
+        create: (context) => AuthService(),
+        child: GetMaterialApp(
+          title: FlavorSettings.title,
+          getPages: PagesRoutes.pages,
+          debugShowCheckedModeBanner: false,
+          theme: Get.find<MyAppThemes>().getLightTheme(),
+          initialRoute: RoutesNames.homePageView,
+        ),
       ),
     );
   }
