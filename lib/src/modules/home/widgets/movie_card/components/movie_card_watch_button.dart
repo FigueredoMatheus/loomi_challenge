0e4b4f675e667376c3932bcd2dd4df3923def157
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-10 04:14:22 -0300

    Implement Watch movie on tap button

--- lib/src/modules/home/widgets/movie_card/components/movie_card_watch_button.dart
+++ lib/src/modules/home/widgets/movie_card/components/movie_card_watch_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:loomi_challenge/src/common/widgets/custom_elevated_button.dart';
+import 'package:loomi_challenge/src/core/routes/routes_names.dart';
 import 'package:loomi_challenge/src/models/entity/movie_entity/movie_entity.dart';
 import 'package:loomi_challenge/src/modules/home/widgets/custon_loading_shimmer_style.dart';
 
@@ -19,7 +21,13 @@ class MovieCardWatchButton extends StatelessWidget {
         child: isLoading
             ? CustomLoadingShimmerStyle(
                 width: 207, height: 41.58, bottomPadding: 0)
-            : CustomElevatedButton(label: 'Watch', onPressed: () async {}),
+            : CustomElevatedButton(
+                label: 'Watch',
+                onPressed: () => Get.toNamed(
+                  RoutesNames.moviePlayerPageView,
+                  arguments: movie,
+                ),
+              ),
       ),
     );
   }
