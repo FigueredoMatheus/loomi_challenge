31d99acebfe03a77c9d4917de0cd935cf2fe6b39
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-10 04:41:41 -0300

    Implement Firebase getMovieCommentsData

--- lib/src/repositories/firebase_database_repository/firebase_database_repository.dart
+++ lib/src/repositories/firebase_database_repository/firebase_database_repository.dart
@@ -3,21 +3,15 @@ import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:path/path.dart';
 part 'post_user_data.dart';
-part 'get_user_data.dart';
+part 'get_comments_data.dart';
 part 'upload_file.dart';
 
 class FirebaseDatabaseRepository {
-  Future<Map<String, dynamic>> GetUserData(String userId) async =>
-      implGetUserData(userId);
+  Future<Map<String, dynamic>> getMovieCommentsData(int movieId) async =>
+      implGetMovieCommentsData(movieId);
 
-  Future<Map<String, dynamic>> postUserData(
+  Future<Map<String, dynamic>> postMovieCommentData(
     Map<String, dynamic> data,
   ) async =>
       implPostUserData(data);
-
-  Future<Map<String, dynamic>> uploadFile({
-    required String userId,
-    required String imagePath,
-  }) async =>
-      implUploadFile(userId: userId, filePath: imagePath);
 }
