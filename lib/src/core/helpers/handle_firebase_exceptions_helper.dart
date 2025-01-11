10add6d2aaeded01c441fb3c7ce792be113d1260
FigueredoMatheus <matheusfigueredob@gmail.com>
2025-01-11 20:15:34 -0300

    Implement handle firebase exceptions

--- lib/src/core/helpers/handle_firebase_exceptions_helper.dart
+++ lib/src/core/helpers/handle_firebase_exceptions_helper.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class HandleFirebaseExceptionsHelper {
-  static String getExceptionMessage(FirebaseAuthException exception) {
+  static String getAuthExceptionMessage(FirebaseAuthException exception) {
     switch (exception.code) {
       case 'user-not-found':
         return 'No user found for that email.';
@@ -16,6 +16,25 @@ class HandleFirebaseExceptionsHelper {
       case 'weak-password':
         return 'The password provided is too weak.';
 
+      default:
+        return 'Unknown error';
+    }
+  }
+
+  static String getFirebaseException(FirebaseException exception) {
+    switch (exception.code) {
+      case 'not-found':
+        return 'The requested document does not exist.';
+      case 'permission-denied':
+        return 'Missing or insufficient permissions.';
+      case 'resource-exhausted':
+        return 'Resource quota exceeded or system overloaded.';
+      case 'unavailable':
+        return 'The service is currently unavailable.';
+      case 'unauthorized':
+        return 'User doesn\'t have permission to access the object.';
+      case 'cancelled':
+        return 'The operation was canceled.';
       default:
         return 'Unknown error';
     }
