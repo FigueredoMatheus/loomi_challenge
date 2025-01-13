51d84e7eb4fe05c24a3462b68884b41a00bb997a
FigueredoMatheus <matheusfigueredob@gmail.com>
2025-01-13 19:31:09 -0300

    Create CommentPageTrailingHeader

---
+++ lib/src/modules/comment/widgets/page_header/trailing_header.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:get/get.dart';
+import 'package:loomi_challenge/src/common/widgets/close_modal_button.dart';
+import 'package:loomi_challenge/src/core/routes/routes_names.dart';
+import 'package:loomi_challenge/src/core/services/get_it.dart';
+import 'package:loomi_challenge/src/core/themes/app_themes.dart';
+import 'package:loomi_challenge/src/modules/comment/store/comment_store.dart';
+
+class CommentPageTrailingHeader extends StatefulWidget {
+  const CommentPageTrailingHeader({super.key});
+
+  @override
+  State<CommentPageTrailingHeader> createState() =>
+      _CommentPageTrailingHeaderState();
+}
+
+class _CommentPageTrailingHeaderState extends State<CommentPageTrailingHeader> {
+  late bool isMovieCommentsPageView;
+  late CommentStore commentStore;
+  @override
+  void initState() {
+    super.initState();
+    commentStore = getIt<CommentStore>();
+
+    isMovieCommentsPageView =
+        Get.currentRoute == RoutesNames.MOVIE_COMMENTS_PAGE_VIEW;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (context) {
+        return commentStore.editCommentMode
+            ? cancelEditCommentBtn()
+            : isMovieCommentsPageView
+                ? CommentPageTextCommentsInfo()
+                : moviePlayerCloseCommentsBtn();
+      },
+    );
+  }
+
+  Widget moviePlayerCloseCommentsBtn() {
+    return CustomCloseButton();
+  }
+
+  Widget cancelEditCommentBtn() {
+    return TextButton(
+      onPressed: commentStore.cancelEditMode,
+      child: Text(
+        'Cancel',
+        style: MyThemes.get().epilogueStyle.copyWith(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+              color: MyThemes.get().kPurpleColor,
+            ),
+      ),
+    );
+  }
+
+  Widget CommentPageTextCommentsInfo() {
+    return Text(
+      '${commentStore.commentsCount} Comments',
+      style: MyThemes.get().epilogueStyle.copyWith(
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+            color: MyThemes.get().kWhiteColor,
+          ),
+    );
+  }
+}
