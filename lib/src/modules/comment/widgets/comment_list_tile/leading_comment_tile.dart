2acbc104c85e2143158bf6554577f9f19d741737
FigueredoMatheus <matheusfigueredob@gmail.com>
2025-01-14 09:27:40 -0300

    Implements the size of the profile image according to the page

--- lib/src/modules/comment/widgets/comment_list_tile/leading_comment_tile.dart
+++ lib/src/modules/comment/widgets/comment_list_tile/leading_comment_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:loomi_challenge/src/common/widgets/images_widgets/circle_avatar_profile_image.dart';
+import 'package:loomi_challenge/src/core/routes/routes_names.dart';
 import 'package:loomi_challenge/src/models/entity/movie_comment_entity/movie_comment_entity.dart';
 
 class LeadingCommentTile extends StatefulWidget {
@@ -12,11 +14,20 @@ class LeadingCommentTile extends StatefulWidget {
 }
 
 class _LeadingCommentTileState extends State<LeadingCommentTile> {
+  late bool isMovieCommentsPageView;
+  @override
+  void initState() {
+    super.initState();
+
+    isMovieCommentsPageView =
+        Get.currentRoute == RoutesNames.MOVIE_COMMENTS_PAGE_VIEW;
+  }
+
   @override
   Widget build(BuildContext context) {
     return CircleAvatarProfileImage(
       user: widget.comment.user,
-      containerSize: 50,
+      containerSize: isMovieCommentsPageView ? 50 : 35,
     );
   }
 }
