6d60e2a7018b3ca837650ccf849167e0a45a5f98
FigueredoMatheus <matheusfigueredob@gmail.com>
2025-01-14 18:31:25 -0300

    Implement update home movie comments when left comments page

--- lib/src/modules/comment/view/movie_comments_page_view.dart
+++ lib/src/modules/comment/view/movie_comments_page_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loomi_challenge/src/core/services/get_it.dart';
 import 'package:loomi_challenge/src/models/entity/movie_entity/movie_entity.dart';
 import 'package:loomi_challenge/src/modules/comment/store/comment_store.dart';
 import 'package:loomi_challenge/src/modules/comment/widgets/comments_body.dart';
+import 'package:loomi_challenge/src/modules/home/bloc/home_bloc/home_page_bloc.dart';
 
 class MovieCommentsPageView extends StatefulWidget {
   final MovieEntity movie;
@@ -31,8 +33,14 @@ class _MovieCommentsPageViewState extends State<MovieCommentsPageView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: MovieCommentsBody(movie: widget.movie),
+    return PopScope(
+      canPop: true,
+      onPopInvoked: (_) => context
+          .read<HomePageBloc>()
+          .add(UpdateMovieCommentsEvent(comments: commentStore.comments)),
+      child: Scaffold(
+        body: MovieCommentsBody(movie: widget.movie),
+      ),
     );
   }
 }
